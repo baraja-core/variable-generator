4345b98c6f7aec57daf49732620730095064b16b
Jan Barášek <janbarasek@gmail.com>
2021-01-19 10:36:53 +0100

    YearPrefixIncrementStrategy: Fix support for starting zeros.

--- src/Strategy/YearPrefixIncrementStrategy.php
+++ src/Strategy/YearPrefixIncrementStrategy.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Baraja\VariableGenerator\Strategy;
 
 
-final class YearPrefixIncrementStrategy implements IVariableGeneratorStrategy
+final class YearPrefixIncrementStrategy implements FormatStrategy
 {
-	private int $length;
+	private ?int $length;
+
+	private int $preferredLength;
 
 
-	public function __construct(int $length = 8)
+	public function __construct(?int $length = null, int $preferredLength = 8)
 	{
 		$this->length = $length;
+		$this->preferredLength = $preferredLength;
 	}
 
 
@@ -20,7 +23,8 @@ final class YearPrefixIncrementStrategy implements IVariableGeneratorStrategy
 	{
 		if (preg_match('/^(?<year>\d{2})(?<count>\d+)$/', $last, $variableParser)) {
 			if (($year = date('y')) === $variableParser['year']) {
-				$new = $variableParser['year'] . str_pad((string) ($variableParser['count'] + 1), $this->length - 2, '0', STR_PAD_LEFT);
+				$length = $this->length ?? \strlen($variableParser['count']);
+				$new = $variableParser['year'] . str_pad((string) ($variableParser['count'] + 1), $length, '0', STR_PAD_LEFT);
 			} else {
 				$new = $year . str_repeat('0', \strlen($variableParser['count']) - 1) . '1';
 			}
@@ -34,6 +38,6 @@ final class YearPrefixIncrementStrategy implements IVariableGeneratorStrategy
 
 	public function getFirst(): string
 	{
-		return date('y') . str_repeat('0', $this->length - 3) . '1';
+		return date('y') . str_repeat('0', ($this->length ?? $this->preferredLength) - 3) . '1';
 	}
 }
